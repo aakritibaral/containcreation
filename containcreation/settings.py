f8af1f930b48e0193b06fea24663014cb70c2707
aakritibaral <baralaakritibaral@gmail.com>
2025-02-27 13:15:18 +0545

    Added static file

--- containcreation/settings.py
+++ containcreation/settings.py
@@ -121,10 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'blog:home'
